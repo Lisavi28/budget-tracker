aa2a2c817d085e54314257517fc2b86c12c47eb3
lisavi <lisavi_28@yahoo.com.mx>
2022-07-02 12:42:48 -0500

    nothing

--- server.js
+++ server.js
@@ -19,10 +19,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+
 
 // routes
 app.use(require("./routes/api.js"));
